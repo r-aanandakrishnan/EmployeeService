898ae34886859a3bf4ce1dae1f01a7d0f5586cd2
r-aanandakrishnan <r.aanandakrishnan@gmail.com>
2020-09-29 09:42:57 +0530

    logger added

--- src/main/java/com/cg/pecuniabank/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/cg/pecuniabank/employeeservice/controller/EmployeeController.java
@@ -1,5 +1,4 @@
 package com.cg.pecuniabank.employeeservice.controller;
-
 import com.cg.pecuniabank.employeeservice.entity.Employee;
 import com.cg.pecuniabank.employeeservice.exception.EmployeeNotFoundException;
 import com.cg.pecuniabank.employeeservice.exception.ReportGenerationException;
@@ -9,7 +8,8 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 
@@ -18,11 +18,13 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+    Logger logger=LoggerFactory.getLogger(EmployeeController.class);
     @GetMapping(
             produces = "application/json",
             headers = "Accept=application/json"
     )
     public List<Employee> getAllEmployees(){
+        logger.info("Displaying all the employees");
         return employeeService.getAllEmployees();
     }
     @GetMapping(
@@ -31,6 +33,7 @@ public class EmployeeController {
             headers = "Accept=application/json"
     )
     public Employee getEmployeeWithId(@PathVariable Long id) throws EmployeeNotFoundException {
+        logger.info("Fetching employee with EmpId"+id);
         return employeeService.getEmployeeById(id);
     }
     @PostMapping(
@@ -38,6 +41,7 @@ public class EmployeeController {
             headers = "Accept=application/json"
     )
     public Employee addNewEmployee(@RequestBody Employee employee) throws Exception{
+        logger.info("Adding new Employee to the database");
         return employeeService.addEmployee(employee);
     }
     @DeleteMapping(
@@ -46,6 +50,7 @@ public class EmployeeController {
             headers = "Accept=application/json"
     )
     public Employee removeEmployee(@PathVariable long id){
+        logger.info("deleting employee with EmpId"+id);
         return employeeService.deleteEmployee(id);
     }
     @PutMapping(
@@ -54,12 +59,14 @@ public class EmployeeController {
             headers = "Accept=application/json"
     )
     public Employee updateEmployee(@PathVariable long id,@RequestBody Employee modifiedEmployee) throws Exception{
+        logger.info("updating employee with EmpNo"+id);
         return employeeService.updateEmployee(id,modifiedEmployee);
     }
     @GetMapping(
             value = "/report"
     )
     public ResponseEntity<InputStreamResource> getAssetReport() throws ReportGenerationException {
+        logger.info("Generating report");
         ByteArrayInputStream in = employeeService.generateAssetReport();
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition","attachment; filename=employee_report.xlsx");
